52101149ce2db0cc0b8f139dc5725f6b48a5f592
SivapriyaManikandan <way2priyamail@gmail.com>
2016-09-04 20:53:44 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,15 @@
+var sum = 0;
+var i;
+for (i=0;i<=1000;i++)
+    {
+        sum = sum + i;
+    }
+console.log(sum);
+i = 1;
+sum = 0;
+while(i<=1000)
+    {
+        sum = sum + i;
+          i++;  }
+console.log(sum);
+alert("finding sum");
